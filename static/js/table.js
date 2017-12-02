ef7cfeeedf9bbcf2c1ae4cf467c3233e9bab2182
Miguel Sanchez <mangelsr@live.com>
2017-12-02 00:46:52 -0500

    [ADD] Archivos para prueba de descarga

--- static/js/table.js
+++ static/js/table.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
     $('#dataTable').DataTable( {
-        "language": {
-            "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
-        },
-        "ajax": 'data/repo.json'
-    } );
+        "language": {"url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"},
+        "ajax": 'data/repo.json',
+        "columns": [
+            { "data": "Facultad" },
+            { "data": "Materia" },
+            { "data": "Recurso",
+              "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                    $(nTd).html("<a href='data/files/" + oData.Facultad + "/" + oData.Materia + "/" + oData.Recurso + ".pdf' download>" + oData.Recurso + "</a>");
+                }
+            },
+            { "data": "Autor" }
+        ]
+    });
 });
