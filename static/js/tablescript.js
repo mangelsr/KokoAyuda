c3d3f36057f8c803f75f93acbce75ff44b8c3868
Miguel Sanchez <mangelsr@live.com>
2017-12-04 09:18:14 -0500

    [ADD] Responsive datatable

--- static/js/tablescript.js
+++ static/js/tablescript.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('#dataTable').DataTable( {
+        "responsive": true,
         "language": {"url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"},
         "ajax": 'data/repo.json',
         "columns": [
